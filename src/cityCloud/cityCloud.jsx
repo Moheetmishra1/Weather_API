89ef85adf8312b32301a2db48326536c3f39b5e5
Mohit Mishra <mohit.m@idexcel.com>
2024-11-07 18:57:43 +0530

    ksehfkjwesf

--- src/cityCloud/cityCloud.jsx
+++ src/cityCloud/cityCloud.jsx
@@ -1,6 +1,6 @@
 
 
-export function CityCloud({}){
+export function CityCloud({city,countryCode,temp,tempType,max,min,day,wType}){
 
 
     return <>
@@ -18,20 +18,21 @@ export function CityCloud({}){
         <div class="temp">
           <div class="label">
             <sup id="marker"><i class="fas fa-map-marker-alt"></i></sup>
-            <h1 id="name"></h1>
-            <sub id="country"></sub>
+            <h1 id="name">{city}</h1>
+            <sub id="country">{countryCode}</sub>
           </div>
 
-          <h1 id="temp"></h1>
+          <h1 id="temp">{temp}</h1>
+          <sub>{tempType}</sub>
           <div class="min-max">
-            <span id="max"></span>
+            <span id="max">{max}</span>
             <span id="slash">/</span>
-            <span id="min"></span>
+            <span id="min">{min}</span>
             <span style="padding: 0 10px"></span>
-            <span id="day"></span>
+            <span id="day">{day}</span>
           </div>
           <span id="description"
-                ><i class="fas fa-cloud"></i> <span id="desc"></span   ></span>
+                ><i class="fas fa-cloud">{wType}</i> <span id="desc"></span   ></span>
         </div>
       </main>
     
